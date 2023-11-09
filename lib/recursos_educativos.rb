042685b38f0d3772c13e530de08678bf893c5ac8
VictorCanovasDelPino <alu0101221953@ull.edu.es>
2023-11-09 16:05:46 +0000

    Comentarios de la clase actividad

--- lib/recursos_educativos.rb
+++ lib/recursos_educativos.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "recursos_educativos/version"
+require_relative "recursos_educativos/actividad"
+require_relative "recursos_educativos/dificultad"
 
 module RecursosEducativos
   class Error < StandardError; end
-  # Your code goes here...
+
+  Actividad = Actividad.new(1, 'Marca', 'Titulo','Descripcion', :beginner, 'Tipo', 'Categoria', 'Material', 45, 'Conceptos')
+  puts Actividad.duracion_estimada
 end
--- lib/recursos_educativos/actividad.rb
+++ lib/recursos_educativos/actividad.rb
@@ -1,8 +1,25 @@
 require 'recursos_educativos/dificultad'
 
+# @author Victor Canovas del Pino
+#
 class Actividad
-  attr_reader :id, :marca, :titulo, :descripcion, :nivel, :tipo, :categoria, :material, :duracion, :conceptos
+  
+  
+  attr_accessor :id, :marca, :titulo, :descripcion, :nivel, :tipo, :categoria, :material, :duracion, :conceptos
 
+  # Constructor de la clase Actividad
+  #
+  # @param id [Integer] Identificador de la actividad.
+  # @param marca [String] Marca de la actividad.
+  # @param titulo [String] Título de la actividad.
+  # @param descripcion [String] Descripción de la actividad.
+  # @param nivel [Symbol] Nivel de experiencia de la actividad.
+  # @param tipo [String] Tipo de actividad.
+  # @param categoria [String] Categoría del recurso.
+  # @param material [String] Material necesario para realizar la actividad.
+  # @param duracion [Integer] Duración de la actividad.
+  # @param conceptos [String] Conceptos que se trabajan en la actividad.
+  #
   def initialize(id, marca, titulo, descripcion, nivel, tipo, categoria, material, duracion, conceptos)
     @id = id
     @marca = marca
@@ -20,6 +37,10 @@ class Actividad
     @conceptos = conceptos
   end 
 
+  # Método para comprobar que la duración es un número mayor que 0
+  # @param duracion [Integer] Duración de la actividad.
+  # @return [Boolean] Devuelve true si la duración es un número mayor que 0, false en caso contrario.
+  #
   def duracion_check(duracion)
     if duracion.is_a? Numeric
       if duracion > 0
@@ -32,10 +53,14 @@ class Actividad
     end
   end
 
+  # Método para mostrar la información de la actividad
+  # @return [String] Devuelve la información de la actividad.
   def to_s
     "Identificador: #{@id}\nMarca: #{@marca}\nTítulo: #{@titulo}\nDescripción: #{@descripcion}\nNivel de experiencia: #{@nivel}\nTipo de actividad: #{@tipo}\nCategoría del recurso: #{@categoria}\nMaterial necesario: #{@material}\nDuración: #{@duracion} minutos\nConceptos: #{@conceptos}"
   end
 
+  # Método para mostrar la duración de la actividad
+  # @return [String] Devuelve la duración de la actividad.
   def duracion_estimada
     "#{@duracion} minutos"      
   end
